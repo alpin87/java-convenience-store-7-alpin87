0fb6b92f228c551a07e479bc1afc02bdf902857c
alpin87 <qortmdals94@naver.com>
2024-11-07 20:15:27 +0900

    feat: 상황에 맞는 예외 enum 추가

--- src/main/java/store/exception/ErrorCode.java
+++ src/main/java/store/exception/ErrorCode.java
@@ -5,7 +5,13 @@ public enum ErrorCode {
     MEMBERSHIP_YES_OR_NO_CHECK("[ERROR] Y 또는 N만 입력 가능합니다."),
     FAILED_TO_READ_PRODUCT("[ERROR] 상품 정보를 불러오는데 실패했습니다."),
     FAILED_TO_READ_PROMOTION("[ERROR] 프로모션 정보를 불러오는데 실패했습니다."),
-    NON_EXISTENT_PRODUCT("[ERROR] 존재하지 않는 상품입니다.");
+    NON_EXISTENT_PRODUCT("[ERROR] 존재하지 않는 상품입니다."),
+    ORDER_DOES_NOT_EXIST("[ERROR] 주문 내역이 존재하지 않습니다."),
+    QUANTITY_SHOULD_BE_POSITIVE("[ERROR] 수량은 1개 이상이어야 합니다."),
+    INVALID_PROMOTION_NAME("[ERROR] 프로모션 이름이 유효하지 않습니다."),
+    INVALID_PROMOTION_QUANTITY("[ERROR] 프로모션 수량이 유효하지 않습니다."),
+    PROMOTION_DATE_INVALID("[ERROR] 프로모션 기간이 유효하지 않습니다."),
+    START_DATE_SHOULD_BE_BEFORE_END_DATE("[ERROR] 시작 날짜는 종료 날짜보다 이전이어야 합니다.");
 
     private final String message;
 
