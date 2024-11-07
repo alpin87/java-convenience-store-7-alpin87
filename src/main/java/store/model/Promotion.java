a73036f43b885420f7b858293ef1e6e9119e68ec
alpin87 <qortmdals94@naver.com>
2024-11-07 20:18:13 +0900

    feat: 세부 유효성 검증 로직 추가

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,6 +1,7 @@
 package store.model;
 
 import java.time.LocalDate;
+import store.exception.ErrorCode;
 
 public class Promotion {
     private final String name;
@@ -11,6 +12,10 @@ public class Promotion {
 
     public Promotion(String name, int buyQuantity, int getFreeQuantity,
                      LocalDate startDate, LocalDate endDate) {
+        validateName(name);
+        validateQuantities(buyQuantity, getFreeQuantity);
+        validateDates(startDate, endDate);
+
         this.name = name;
         this.buyQuantity = buyQuantity;
         this.getFreeQuantity = getFreeQuantity;
@@ -32,5 +37,34 @@ public class Promotion {
 
     public boolean isValid(LocalDate date) {
         return !date.isBefore(startDate) && !date.isAfter(endDate);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_PROMOTION_NAME.getMessage());
+        }
+    }
+
+    private void validateQuantities(int buyQuantity, int getFreeQuantity) {
+        if (buyQuantity <= 0 || getFreeQuantity <= 0) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_PROMOTION_QUANTITY.getMessage());
+        }
+    }
+
+    private void validateDates(LocalDate startDate, LocalDate endDate) {
+        validateDateNotNull(startDate, endDate);
+        validateDateRange(startDate, endDate);
+    }
+
+    private void validateDateNotNull(LocalDate startDate, LocalDate endDate) {
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException(ErrorCode.PROMOTION_DATE_INVALID.getMessage());
+        }
+    }
+
+    private void validateDateRange(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException(ErrorCode.START_DATE_SHOULD_BE_BEFORE_END_DATE.getMessage());
+        }
     }
 }
