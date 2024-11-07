958c44c48451ff92a89be43bf23a21027c218616
alpin87 <qortmdals94@naver.com>
2024-11-07 20:17:24 +0900

    feat: Order클래스 상품과 수량 유효성 검증 로직 추가

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -1,10 +1,13 @@
 package store.model;
 
+import store.exception.ErrorCode;
+
 public class Order {
     private final Product product;
     private final int quantity;
 
     public Order(Product product, int quantity) {
+        validateOrder(product, quantity);
         this.product = product;
         this.quantity = quantity;
     }
@@ -19,5 +22,14 @@ public class Order {
 
     public int calculateTotalPrice() {
         return product.getPrice() * quantity;
+    }
+
+    private void validateOrder(Product product, int quantity) {
+        if (product == null) {
+            throw new IllegalArgumentException(ErrorCode.NON_EXISTENT_PRODUCT.getMessage());
+        }
+        if (quantity <= 0) {
+            throw new IllegalArgumentException(ErrorCode.QUANTITY_SHOULD_BE_POSITIVE.getMessage());
+        }
     }
 }
