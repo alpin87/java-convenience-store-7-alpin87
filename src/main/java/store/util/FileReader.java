5dfb50c3b85211609e81e21a2908d0c5e2373c1d
alpin87 <qortmdals94@naver.com>
2024-11-07 18:39:56 +0900

    feat: 파일 경로 상수 정의

---
+++ src/main/java/store/util/FileReader.java
@@ -0,0 +1,6 @@
+package store.util;
+
+public class FileReader {
+    private static final String PRODUCTS_PATH = "src/main/resources/products.md";
+    private static final String PROMOTIONS_PATH = "src/main/resources/promotions.md";
+}
