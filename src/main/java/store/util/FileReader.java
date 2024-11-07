0c15cd44bf301443935ef6b4bab20ae5e20ff803
alpin87 <qortmdals94@naver.com>
2024-11-07 18:41:14 +0900

    feat: 파일 읽기 기본메소드 추가

--- src/main/java/store/util/FileReader.java
+++ src/main/java/store/util/FileReader.java
@@ -1,6 +1,28 @@
 package store.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import store.exception.ErrorCode;
+
 public class FileReader {
     private static final String PRODUCTS_PATH = "src/main/resources/products.md";
     private static final String PROMOTIONS_PATH = "src/main/resources/promotions.md";
+
+    public List<String> readProducts() {
+        try {
+            return Files.readAllLines(Paths.get(PRODUCTS_PATH));
+        } catch (IOException e) {
+            throw new IllegalStateException(ErrorCode.NOT_FOUND_PRODUCT.getMessage());
+        }
+    }
+
+    public List<String> readPromotions() {
+        try {
+            return Files.readAllLines(Paths.get(PROMOTIONS_PATH));
+        } catch (IOException e) {
+            throw new IllegalStateException(ErrorCode.NOT_FOUND_PROMOTION.getMessage());
+        }
+    }
 }
