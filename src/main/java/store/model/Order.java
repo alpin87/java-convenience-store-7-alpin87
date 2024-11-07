f030ba1f3f99d26d76e19368ea8c08245b8f8580
alpin87 <qortmdals94@naver.com>
2024-11-07 18:38:21 +0900

    feat: 물건 구매를 위한 Order class 추가

---
+++ src/main/java/store/model/Order.java
@@ -0,0 +1,23 @@
+package store.model;
+
+public class Order {
+    private final Product product;
+    private final int quantity;
+
+    public Order(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int calculateTotalPrice() {
+        return product.getPrice() * quantity;
+    }
+}
