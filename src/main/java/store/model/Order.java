8456293d0a083e640c0a3ce82114e26a19ec6e9b
alpin87 <qortmdals94@naver.com>
2024-11-10 01:33:55 +0900

    feat: 주문 정보 관리 및 가격 계산 로직 구현

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -1,15 +1,27 @@
 package store.model;
 
 import store.exception.ErrorCode;
+import store.model.Product;
+import store.validator.Validator;
 
 public class Order {
     private final Product product;
     private final int quantity;
+    private final boolean isPromotional;
 
-    public Order(Product product, int quantity) {
-        validateOrder(product, quantity);
+    public Order(Product product, int quantity, boolean isPromotional) {
+        validateOrderDetails(product, quantity);
         this.product = product;
         this.quantity = quantity;
+        this.isPromotional = isPromotional;
+    }
+
+    public boolean isPromotional() {
+        return isPromotional;
+    }
+    private void validateOrderDetails(Product product, int quantity) {
+        Validator.validateNotNull(product, ErrorCode.NON_EXISTENT_PRODUCT);
+        Validator.validatePositiveNumber(quantity);
     }
 
     public Product getProduct() {
@@ -22,14 +34,5 @@ public class Order {
 
     public int calculateTotalPrice() {
         return product.getPrice() * quantity;
-    }
-
-    private void validateOrder(Product product, int quantity) {
-        if (product == null) {
-            throw new IllegalArgumentException(ErrorCode.NON_EXISTENT_PRODUCT.getMessage());
-        }
-        if (quantity <= 0) {
-            throw new IllegalArgumentException(ErrorCode.QUANTITY_SHOULD_BE_POSITIVE.getMessage());
-        }
     }
 }
