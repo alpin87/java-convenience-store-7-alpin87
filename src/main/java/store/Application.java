df7d4d28039594e3fc8019e20ed132736e0718fc
alpin87 <qortmdals94@naver.com>
2024-11-10 01:41:26 +0900

    feat: 애플리케이션 초기화 및 설정 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,27 @@
 package store;
 
+import store.controller.StoreController;
+import store.service.OrderService;
+import store.service.ProductService;
+import store.util.FileReader;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        FileReader fileReader = new FileReader();
+        ProductService productService = new ProductService(fileReader);
+        OrderService orderService = new OrderService(productService);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        StoreController storeController = new StoreController(
+                productService,
+                orderService,
+                inputView,
+                outputView
+        );
+
+        storeController.run();
     }
 }
