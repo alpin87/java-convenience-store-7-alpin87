33a1b7b8d13d84e21ea032524f15c624cfaee44b
alpin87 <qortmdals94@naver.com>
2024-11-18 20:37:05 +0900

    feat: 재고관리를위한 로직추가

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -1,24 +1,26 @@
 package store.model;
 
 import store.exception.ErrorCode;
-import store.model.Product;
 import store.validator.Validator;
 
 public class Order {
     private final Product product;
     private final int quantity;
     private final boolean isPromotional;
+    private final boolean isPromotionalGift;
 
     public Order(Product product, int quantity, boolean isPromotional) {
+        this(product, quantity, isPromotional, false);
+    }
+
+    public Order(Product product, int quantity, boolean isPromotional, boolean isPromotionalGift) {
         validateOrderDetails(product, quantity);
         this.product = product;
         this.quantity = quantity;
         this.isPromotional = isPromotional;
+        this.isPromotionalGift = isPromotionalGift;
     }
 
-    public boolean isPromotional() {
-        return isPromotional;
-    }
     private void validateOrderDetails(Product product, int quantity) {
         Validator.validateNotNull(product, ErrorCode.NON_EXISTENT_PRODUCT);
         Validator.validatePositiveNumber(quantity);
@@ -32,7 +34,18 @@ public class Order {
         return quantity;
     }
 
+    public boolean isPromotional() {
+        return isPromotional;
+    }
+
+    public boolean isPromotionalGift() {
+        return isPromotionalGift;
+    }
+
     public int calculateTotalPrice() {
+        if (isPromotionalGift) {
+            return 0;
+        }
         return product.getPrice() * quantity;
     }
 }
